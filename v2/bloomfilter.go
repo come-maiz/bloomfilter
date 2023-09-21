3f80b78a22ae0f1fb36faf094129fc92c837443e
Martin Holst Swende <martin@swende.se>
2023-09-21 14:40:48 +0200

    filter: more correct locking

--- v2/bloomfilter.go
+++ v2/bloomfilter.go
@@ -111,13 +111,16 @@ func (f *Filter) Copy() (*Filter, error) {
 
 // UnionInPlace merges Bloom filter f2 into f
 func (f *Filter) UnionInPlace(f2 *Filter) error {
+	if f == f2 {
+		return nil
+	}
 	if !f.IsCompatible(f2) {
 		return errors.New("incompatible bloom filters")
 	}
-
 	f.lock.Lock()
 	defer f.lock.Unlock()
-
+	f2.lock.RLock()
+	defer f2.lock.RUnlock()
 	for i, bitword := range f2.bits {
 		f.bits[i] |= bitword
 	}
@@ -128,10 +131,12 @@ func (f *Filter) UnionInPlace(f2 *Filter) error {
 
 // Union merges f2 and f2 into a new Filter out
 func (f *Filter) Union(f2 *Filter) (out *Filter, err error) {
+	if f == f2 {
+		return f.Copy()
+	}
 	if !f.IsCompatible(f2) {
 		return nil, errors.New("incompatible bloom filters")
 	}
-
 	f.lock.RLock()
 	defer f.lock.RUnlock()
 
@@ -139,6 +144,9 @@ func (f *Filter) Union(f2 *Filter) (out *Filter, err error) {
 	if err != nil {
 		return nil, err
 	}
+	f2.lock.RLock()
+	defer f2.lock.RUnlock()
+
 	for i, bitword := range f2.bits {
 		out.bits[i] = f.bits[i] | bitword
 	}
@@ -152,7 +160,7 @@ func (f *Filter) Clear() {
 	f.lock.Lock()
 	defer f.lock.Unlock()
 
-	for i, bitword := range f.bits {
+	for i := range f.bits {
 		f.bits[i] = 0
 	}
 	f.n = 0 // Also update the counters
